d0164ba5b4d9fc692f34999b7ffbb148b3d4c045
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-01 19:01:29 +0530

    implementing products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Depot::Application.routes.draw do
-  resources :products
+  get "store/index"
 
+  resources :products
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -27,6 +29,7 @@ Depot::Application.routes.draw do
   #     end
   #   end
 
+
   # Sample resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
@@ -51,7 +54,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+    root to: 'store#index', as: 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
